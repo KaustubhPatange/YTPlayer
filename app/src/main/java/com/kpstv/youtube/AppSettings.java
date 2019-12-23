ef1c0634f6e3faab8c667e30d710ac6eb8df06a9
KaustubhPatange <developerkp16@gmail.com>
2019-12-23 20:25:04 +0530

    Added Popular section

---
+++ app/src/main/java/com/kpstv/youtube/AppSettings.java
@@ -0,0 +1,9 @@
+package com.kpstv.youtube;
+
+import com.kpstv.youtube.utils.SortOrder;
+import com.kpstv.youtube.utils.SortType;
+
+public class AppSettings {
+    public static SortOrder sortOrder;
+    public static SortType sortType;
+}
